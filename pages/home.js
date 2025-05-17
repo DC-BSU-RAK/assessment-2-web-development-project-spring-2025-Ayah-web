644b042bf51b31efaca0479c336b217c447f872a
Ayahtullah Centeno <Ayahfahmy2007@gmail.com>
2025-05-17 20:29:38 +0400

    added comments to code

--- pages/home.js
+++ pages/home.js
@@ -1,13 +1,21 @@
+// load dom fully before executing js
 document.addEventListener('DOMContentLoaded', function() {
   
+    // get ref to element
     const exitBtn = document.querySelector('.exit-btn');
+    // exit button function
     if (exitBtn) {
         exitBtn.addEventListener('click', function() {
+
+            // confirmation dialog
             if (confirm('Do you want to close this window?')) {
                 const welcomeTab = document.querySelector('.welcome-tab');
+
+                // fade out transition
                 welcomeTab.style.transition = 'opacity 0.3s ease';
                 welcomeTab.style.opacity = '0';
                 
+                // after fade, element is hidden
                 setTimeout(() => {
                     welcomeTab.style.display = 'none';
                 }, 300);
